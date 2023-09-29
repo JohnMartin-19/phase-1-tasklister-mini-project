589470f0aa8fa6f9836dc9c2b39ab71c8e9352e6
John Mburu <johnnkonge2020@gmail.com>
2023-09-29 09:47:12 +0300

    trial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const click = document.getElementById('submit')
+  click.addEventListener('click', function(event){
+    //let submit = 'preventDefault.'
+    event.preventDefault();
+    return 'defaultPrevented';
+  });
 });
